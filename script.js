4083d00541a42e3e1614e42f31df9cda68d424ec
RAJA <rajerajendran7777@gmail.com>
2022-02-22 12:53:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let employee ={
+    firstname: "John",
+    lastname: "Raja",
+    Gender: "Male",
+    Age: '22',
+    location:'Madurai'
+}
+Let{firstname,lastname} = employee;
+
+console.log(firstname,lastname);
+   
